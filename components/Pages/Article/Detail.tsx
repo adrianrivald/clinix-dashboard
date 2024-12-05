4cbd4a5efcf0ed8533d3117da6ab08cf1b53bd82
adrianrivald <adrianrivaldy@gmail.com>
2024-12-05 16:53:11 +0700

    article detail date

--- components/Pages/Article/Detail.tsx
+++ components/Pages/Article/Detail.tsx
@@ -11,11 +11,13 @@ export function ArticleDetailContent() {
     <div className={twMerge("p-8 lg:p-16", maxWidthContainer)}>
       {/* Article Detail */}
       <div id="article-detail" className="mx-36">
+        {/* Article Title */}
         <h1 className="text-[32px] lg:text-[50px] font-bold">
           Studi Temukan Efek Buruk Bagi Mereka yang Pernah Kena COVID, Bikin
           Otak Jadi Gini
         </h1>
-        <div className="flex justify-between items-center mt-8">
+        {/* Date Post */}
+        <div className="flex justify-between items-center mt-8 py-6 border-t-2 border-b-2 border-dashed">
           <span>Dipublish 24 September 2024</span>
           <div className="flex items-center gap-4"></div>
         </div>
