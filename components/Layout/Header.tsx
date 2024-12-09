7b2ee806549419f98ec58111a9ff08bc6fd89ea2
adrianrivald <adrianrivaldy@gmail.com>
2024-12-09 14:43:39 +0700

    change hamburger animation

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Hamburger from "hamburger-react";
+import { Squash as Hamburger } from "hamburger-react";
 
 import { useRouter } from "next/router";
 import React, { Dispatch } from "react";
